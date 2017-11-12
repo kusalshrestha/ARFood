56872553551609c1416c97e0676d0cfa2e8944d4
Kusal <shrstha.kusal@gmail.com>
2017-11-12 11:39:21 +0545

    temp: done

--- ARDemoApp/ViewControllers/Views/ModelTableViewCell.swift
+++ ARDemoApp/ViewControllers/Views/ModelTableViewCell.swift
@@ -13,13 +13,16 @@ class ModelTableViewCell: UITableViewCell {
   @IBOutlet weak var titile: UILabel!
   @IBOutlet weak var collectionView: UICollectionView!
   
+  let cellWidth = UIScreen.main.bounds.width * 0.86
   let collectionViewVM = CollectionViewVM.sharedInstance
   var indexPathOfTableView: IndexPath?
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    collectionView.backgroundColor = UIColor.white
+    
     selectionStyle = .none
+    collectionView.backgroundColor = UIColor.white
+    collectionView.decelerationRate = UIScrollViewDecelerationRateFast
   }
   
   func setCollectionViewDataSourceDelegate(vc: MenuViewController, forIndexPath indexPath: IndexPath) {
@@ -48,12 +51,77 @@ extension ModelTableViewCell: UICollectionViewDataSource {
   
 }
 
+var contentOffsetXWhileDragBegin: CGFloat = 0
+var currentIndexPath: IndexPath?
+
 extension ModelTableViewCell: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
   
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
       let row = indexPathOfTableView?.row
-      return row == 0 ? CGSize(width: 240, height: 200) : CGSize(width: 80, height: 120)
+      return row == 0 ? CGSize(width: cellWidth, height: 200) : CGSize(width: 80, height: 120)
     }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    return CGSize(width: 20, height: 200)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+    return CGSize(width: 20, height: 200)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    var reuseId = ""
+    switch kind {
+    case UICollectionElementKindSectionHeader:
+      reuseId = "reusableHeaderView"
+    case UICollectionElementKindSectionFooter:
+      reuseId = "reusableFooterView"
+    default:
+      return UICollectionReusableView()
+    }
+    let reusableView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: reuseId, for: indexPath)
+    return reusableView
+  }
+  
+  //case for scroll
+  func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+    let contentOffSetXOnDeccelerate = scrollView.contentOffset.x
+    if let indexPath = currentIndexPath {
+      if (contentOffSetXOnDeccelerate - contentOffsetXWhileDragBegin) > 0 {
+        print("left")
+        if indexPath.row < (collectionView.numberOfItems(inSection: 0) - 1) {
+          let newRow = indexPath.row + 1
+          let indexPath = IndexPath(item: newRow, section: 0)
+          collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+      } else {
+        print("right")
+        if indexPath.row > 0 {
+          let newRow = indexPath.row - 1
+          let indexPath = IndexPath(item: newRow, section: 0)
+          collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+      }
+//      let row = Int(scrollView.contentOffset.x / cellWidth)
+//      let indexPath = IndexPath(item: newRow, section: 0)
+//      collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+  }
+  
+  // Case for drag
+  
+  func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+    contentOffsetXWhileDragBegin = scrollView.contentOffset.x
+    currentIndexPath = collectionView.indexPathForItem(at: CGPoint(x: collectionView.bounds.width / 2 + collectionView.contentOffset.x, y: 50))
+  }
+  
+//  func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+////    let contentOffsetOnEndDrag = scrollView.contentOffset.x
+//
+//    let row = Int(scrollView.contentOffset.x / cellWidth)
+//    let indexPath = IndexPath(item: row, section: 0)
+//    collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+//  }
   
 }
 
